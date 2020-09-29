fed39d6b9107f64b082056c02120d57f80b2e945
moussbed <moussakatbedril@yahoo.com>
2020-09-29 09:41:32 +0400

    Creer une configuration en fonction de la plateforme en creant la variable d'environnement du port de keycloak

--- frontend-client-side/src/app/services/keycloak-security.service.ts
+++ frontend-client-side/src/app/services/keycloak-security.service.ts
@@ -1,29 +1,30 @@
+import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
 import { KeycloakInstance } from 'keycloak-js';
-declare var Keycloak : any;
+declare var Keycloak: any;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class KeycloakSecurityService {
+  public kc: KeycloakInstance;
 
-  public kc : KeycloakInstance;
+  constructor() {}
 
-  constructor() { }
-
- async init(){
-    console.log("Security Initialisation")
-    this.kc= new Keycloak({
-      url:"http://localhost:8070/auth",
-      realm:"ecom-realm",
-      clientId:"products-client-side"
+  async init() {
+    console.log('Security Initialisation....');
+    console.log(environment.PORT_KEYCLOAK);
+    const port = environment.PORT_KEYCLOAK;
+    this.kc = new Keycloak({
+      url: `http://localhost:${port}/auth`,
+      realm: 'ecom-realm',
+      clientId: 'products-client-side',
     });
 
     await this.kc.init(
-      //{onLoad: "login-required"}
-      {onLoad:"check-sso"}
+      // {onLoad: "login-required"}
+      { onLoad: 'check-sso' }
     );
     console.log(this.kc.token);
-
   }
 }
--- suppliers/src/main/java/com/mb/suppliers/SuppliersApplication.java
+++ suppliers/src/main/java/com/mb/suppliers/SuppliersApplication.java
@@ -7,6 +7,7 @@ import lombok.ToString;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,11 +16,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -54,6 +59,22 @@ class KeycloakConfig {
 	}
 }
 
+@Configuration
+class CorsFilter {
+
+	@Bean
+	public WebMvcConfigurer corsConfiguration() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods(HttpMethod.GET.toString(), HttpMethod.POST.toString(),
+								HttpMethod.PUT.toString(), HttpMethod.DELETE.toString(), HttpMethod.OPTIONS.toString())
+						.allowedOrigins("*");
+			}
+		};
+	}
+}
 @KeycloakConfiguration
 class KeycloakSpringSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 	@Override
